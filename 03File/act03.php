08a5139209dc74f3ec21d633f161f442486172f0
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 10:26:50 +0000

    revised the content

---
+++ 03File/act03.php
@@ -0,0 +1,25 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 01</title>
+</head>
+    <h3>Loop Activity 02</h3>
+<body>
+<?php  
+
+for ($roldan=1; $roldan<=5; $roldan++){
+  for ($hinata=1; $hinata<=$roldan ;$hinata++)	{
+    echo " ♔ ";}
+    echo "<br>";}
+
+
+
+?> 
+
+</body>
+</html>
