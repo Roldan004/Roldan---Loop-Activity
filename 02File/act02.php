86e11c27fe2781d9b3c42f37e1319a2f0a918356
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 10:31:03 +0000

    edit title

--- 02File/act02.php
+++ 02File/act02.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roldan Activity 01</title>
+    <title>Roldan Activity 02</title>
 </head>
     <h3>Loop Activity 02</h3>
 <body>
