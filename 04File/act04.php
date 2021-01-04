d5ceb95ad166fd311d9530a275cc79cbe9216be1
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 10:54:47 +0000

    add activity #4

---
+++ 04File/act04.php
@@ -0,0 +1,28 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 04</title>
+</head>
+    <h3>Loop Activity 04</h3>
+<body>
+<?php  
+
+for ($roldan=1; $roldan<=5; $roldan++){
+  for ($hinata=1; $hinata<=$roldan ;$hinata++)	{
+    echo " ♔ ";}
+    echo "<br>";}
+
+for ($dan=0; $dan<=5; $dan++){
+    for ($hina=5-$dan;$hina>=1;$hina--){
+      echo " ♚ ";}
+      echo "<br>";}
+
+?> 
+
+</body>
+</html>
