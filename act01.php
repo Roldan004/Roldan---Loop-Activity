563a0cddd6629e733b411b35649f7bd68b3ba438
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 06:16:43 +0000

    add 1st file

---
+++ act01.php
@@ -0,0 +1,22 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 01</title>
+</head>
+    <h3>Loop Activity 01</h3>
+<body>
+<?php
+  for ($x = 0; $x <= 10; $x++) {
+    echo $x;
+  }
+
+
+?> 
+</body>
+</html>
+
