efeb42877f9dc575e4ed0f721d2f4716fb22cda2
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 12:59:30 +0000

    add activity 05

---
+++ 05File/act05.php
@@ -0,0 +1,25 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 05</title>
+</head>
+    <h3>Loop Activity 05</h3>
+<body>
+<?php  
+$dan=4;
+$fin=1;
+for($i=1;$i<=$dan;$i++)
+{
+	$fin=$fin*$i;
+} 
+	echo "Factorial=".$fin;
+
+?> 
+
+</body>
+</html>
