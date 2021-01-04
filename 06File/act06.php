fade00fd780282d7ac8a9602ea30db1af09cee34
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 13:00:36 +0000

    add activity 06

---
+++ 06File/act06.php
@@ -0,0 +1,33 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 06</title>
+</head>
+    <h3>Loop Activity 06</h3>
+<body>
+<?php  
+function count_letters($word,$hivelabstechnologies) {
+
+	$word_len = strlen($word) -1;
+    $count = 0;
+    
+   	for($x = 0; $x <= $word_len; $x++) {
+    
+    	if($word[$x] == $hivelabstechnologies) {
+        	$count++;
+        }
+     }
+    echo "The number of ".$hivelabstechnologies." in ".$word." is: ".$count;
+}
+
+count_letters("hivelabstechnologies","e");
+
+?> 
+
+</body>
+</html>
