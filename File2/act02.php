12fac24fb0c14fadda30cdf4c7e10c6349be5537
Roldan004 <roldan.apas04@gmail.com>
2021-01-04 09:45:26 +0000

    add 2nd file

---
+++ File2/act02.php
@@ -0,0 +1,24 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Activity 01</title>
+</head>
+    <h3>Loop Activity 02</h3>
+<body>
+<?php  
+$sum=0;
+for ($x=1; $x<=30; $x++) 
+  {
+     $sum+=$x;
+  }
+    echo "Adding the integer from 1 to 30 the total is $sum" . "\n";
+
+?> 
+
+</body>
+</html>
