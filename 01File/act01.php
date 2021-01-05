359bd3095818d410790490cbb3901b7070054197
Roldan004 <roldan.apas04@gmail.com>
2021-01-05 04:08:53 +0000

    modify content

--- 01File/act01.php
+++ 01File/act01.php
@@ -6,17 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roldan Activity 01</title>
-</head>
+    <title>Roldan Activity 06</title>
+    </head>
     <h3>Loop Activity 01</h3>
 <body>
+    <p>A script that displays 1-2-3-4-5-6-7-8-9-10 on one line.</p>
+    <p>There will be no hyphen(-) at starting and ending position.</p>
+
+    <p>Answer:</p>
 <?php
   for ($x = 0; $x <= 10; $x++) {
     echo $x;
   }
 
-
 ?> 
+
 </body>
 </html>
-
