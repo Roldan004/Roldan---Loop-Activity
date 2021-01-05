9dfc6f02a64c3ba75bd890cf44d28059a8081559
Roldan004 <roldan.apas04@gmail.com>
2021-01-05 10:06:19 +0000

    edit content

--- 05File/act05.php
+++ 05File/act05.php
@@ -6,9 +6,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roldan Activity 05</title>
+<title>Roldan Activity 05</title>
 </head>
     <h3>Loop Activity 05</h3>
+    <p>a program to calculate and print the factorial of a number using a for loop.</p>
+    <p>The factorial of a number is the product of all integers up to and including</p> 
+    <p>that number, so the factorial of 4 is 4*3*2*1= 24.</p>
+    <br>
+    <p>Answer:</p>
 <body>
 <?php  
 $dan=4;
@@ -17,8 +22,7 @@ for($i=1;$i<=$dan;$i++)
 {
 	$fin=$fin*$i;
 } 
-	echo "Factorial=".$fin;
-
+	echo "Factorial=".$fin;    
 ?> 
 
 </body>
